da15a835a65a06c65c6677b3f21778c0fe770332
NetoCruz <galaxy.net0710@gmail.com>
2018-10-06 17:57:30 -0500

    ReactEntorno

---
+++ my-app/src/body.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class Body extends Component {
+  render() {
+    return (
+      <div className="body">
+        <p>hola</p>
+      </div>
+    );
+  }
+}
+
+export default Body;
